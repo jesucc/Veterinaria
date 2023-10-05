77e9dd6b1e800274beb1b084ef366d51e74cc9e4
jesuscc <jesuscamachoforever@gmail.com>
2023-10-05 01:50:45 -0500

    p.1

---
+++ VeterinariaJ/models/Usuario.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'Conexion.php';
+
+class Usuario extends Conexion{
+  
+  private $access;
+
+  public function __CONSTRUCT(){
+    $this->access = parent::getConnection();
+
+  }
+
+  public function login ($dni = ""){
+    try{
+        $query = $this->access->prepare("CALL spu_login(?)");
+        $query->execute(array($dni));
+
+        return $query->fetch(PDO::FETCH_ASSOC);
+
+    }
+    catch(Exception $err){
+        die($err->getMessage());
+
+    }
+
+  }
+
+
+
+}
+
+  $claveacceso = "12345";
+
+  $claveacceso = password_hash($claveacceso, PASSWORD_BCRYPT);
+  echo $claveacceso;
+
+
+
+?>
